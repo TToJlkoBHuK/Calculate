2e5868cb15120bd23601b952eb627bc72b27421b
TToJlkoBHuK <28arhalex@gmail.com>
2025-10-19 22:19:45 +0300

    realization operation pow

---
+++ plugins/funcpow/funcpow.cpp
@@ -0,0 +1,25 @@
+#include "pch.h"
+#include "funcpow.h"
+#include <memory>
+
+double FuncPow::operation(const std::vector<double>& arg) const {
+	if (arg.size() == 2) {
+		return pow(arg[0], arg[1]);
+	}
+	throw ErrorMessage("operation ^ have 2 arg");
+}
+
+const char* FuncPow::getName() {
+	return "^";
+}
+
+extern "C" __declspec(dllexport) double operationE(const std::vector<double>&arg) {
+	std::unique_ptr<BaseOperation> baseoper = std::make_unique<FuncPow>();
+	return baseoper->operation(arg);
+}
+
+extern "C" __declspec(dllexport) const char* getNameE() {
+	std::unique_ptr<BaseOperation> name = std::make_unique<FuncPow>();
+	return name->getName();
+}
+
